f31fc5223a44946f17094eff02c19a3d7b89fed0
mohsin <mohseenkhan468@gmail.com>
2026-04-27 11:43:26 +0530

    feat(header)

---
+++ src/components/Head.jsx
@@ -0,0 +1,36 @@
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-8"
+          src="https://cdn-icons-png.flaticon.com/128/6015/6015685.png"
+          alt="menu"
+        />
+        <img
+          className="h-8 mx-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/3840px-Logo_of_YouTube_%282015-2017%29.svg.png"
+          alt="youtube-logo"
+        />
+      </div>
+      <div className="col-span-10 px-10">
+        <input
+          type="text"
+          className="border border-gray-400 w-1/2 p-2 rounded-l-full"
+        />
+        <button className="border border-gray-400 p-2 rounded-r-full">
+          Search
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="user"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
